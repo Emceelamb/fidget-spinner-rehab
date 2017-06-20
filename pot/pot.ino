916ab1b40a2cbac04e9d0a76290b9735962bb9d2
emceelamb <mlam@berkeley.edu>
2017-06-19 21:27:25 -0400

    'Stuff'

---
+++ pot/pot.ino
@@ -0,0 +1,41 @@
+
+
+  
+int analogPin = 3;     // potentiometer wiper (middle terminal) connected to analog pin 3
+
+                       // outside leads to ground and +5V
+
+int val = 0;           // variable to store the value read
+
+
+
+void setup()
+
+{
+
+  Serial.begin(9600);          //  setup serial
+
+}
+
+
+
+void loop()
+
+{
+
+  val = analogRead(analogPin);    // read the input pin
+
+  Serial.println(val);             // debug value
+
+}
+
+  
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
